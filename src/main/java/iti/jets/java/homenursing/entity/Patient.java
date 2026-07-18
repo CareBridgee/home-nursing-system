bfc0135306dea9a3c9185175578b40191ebced38
ZahraaRagab <alzahraaragab40@gmail.com>
2026-07-18 05:07:09 +0300

    refactor: remove @EqualsAndHashCode from all entities

--- src/main/java/iti/jets/java/homenursing/entity/Patient.java
+++ src/main/java/iti/jets/java/homenursing/entity/Patient.java
@@ -11,7 +11,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,7 +23,6 @@ import java.util.UUID;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
--- src/main/java/iti/jets/java/homenursing/entity/PatientMedication.java
+++ src/main/java/iti/jets/java/homenursing/entity/PatientMedication.java
@@ -11,7 +11,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,7 +22,6 @@ import java.util.UUID;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder

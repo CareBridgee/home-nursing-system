aadb3b909e9135e15dc801ca542c129eed422673
mohammedArabie <mo.arabie83@gmail.com>
2026-07-23 08:44:07 +0300

    feat: auto-detect server URL via forward headers for Swagger

--- src/main/java/iti/jets/java/homenursing/config/OpenApiConfig.java
+++ src/main/java/iti/jets/java/homenursing/config/OpenApiConfig.java
@@ -1,14 +1,12 @@
 package iti.jets.java.homenursing.config;
 
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,12 +29,6 @@ public class OpenApiConfig {
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("https://www.apache.org/licenses/LICENSE-2.0")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("Home Nurse Care Wiki")
-                        .url("https://github.com/anomalyco/home-nurse-care/wiki"))
-                .addServersItem(new Server()
-                        .url("http://localhost:8080")
-                        .description("Local development server"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
                                 .name("bearerAuth")
